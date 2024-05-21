26536b2d904c0b1750fc208c1f5d102f6bad2528
darkmattersol <donalfeelydev@gmail.com>
2024-05-22 03:33:16 +0900

    test:  poker game logic test

--- test/Poker.js
+++ test/Poker.js
@@ -8,124 +8,109 @@ const {
 describe("Test Poker", function () {
 
     async function deployTokenFixture() {
-        // Get the Signers here.
         const [owner, addr1, addr2] = await ethers.getSigners();
-
-        // To deploy our contract, we just have to call ethers.deployContract and await
-        // its waitForDeployment() method, which happens once its transaction has been
-        // mined.
         const rc4Contract = await ethers.getContractFactory("RC4");
         const rc4 = await rc4Contract.deploy();
         const Poker = await ethers.getContractFactory("Poker")
         const poker = await Poker.deploy(await rc4.getAddress());
-
-        // await poker.waitForDeployment();
-        // await rc4.waitForDeployment();
-
-        // Fixtures can return anything you consider useful for your tests
         return { poker, owner, addr1, addr2 };
     }
 
-    it('get cards', async () => {
-        const { poker, owner, addr1, addr2 } = await loadFixture(
-            deployTokenFixture
-        );
-
-        // expect(cards).to.equal(gamid);
-    })
-
-    it('creategame ', async () => {
-        const { poker, owner, addr1, addr2 } = await loadFixture(
-            deployTokenFixture
-        );
-
-        let arr = [];
-
-        for (let i = 1; i <= 52; i++) {
-            arr.push(ethers.hexlify(ethers.toUtf8Bytes(i.toString())));
-        }
-
-        await poker.createGame(arr);
-
-        const ncards = await poker.getCards();
-
-        // expect(cards).to.equal(gamid);
-    })
-
-    it('shuffle encrypt ', async () => {
-
-        const { poker, owner, addr1, addr2 } = await loadFixture(
-            deployTokenFixture
-        );
-
-        let arr = [];
-
-        for (let i = 52; i >= 1; i--) {
-            arr.push(ethers.hexlify(ethers.toUtf8Bytes(i.toString())));
-        }
-
-        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes("key")), arr);
-
-        const ncards = await poker.getCards();
-
-        // expect(cards).to.equal(gamid);
-    })
-
-    it('reveal card ', async () => {
-
-        const { poker, owner, addr1, addr2 } = await loadFixture(
-            deployTokenFixture
-        );
-
-        let arr = [];
-
-        for (let i = 1; i <= 52; i++) {
-            arr.push(ethers.hexlify(ethers.toUtf8Bytes(i.toString())));
-        }
-
-        await poker.createGame(arr);
-
-        let arr1 = [];
-
-        for (let i = 52; i >= 1; i--) {
-            arr1.push(ethers.hexlify(ethers.toUtf8Bytes(i.toString())))
-        }
-
-        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes("key")), arr1);
-
-        const ncards = await poker.getCards();
-        console.log(ncards);
-
-        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes("key")), 1);
-        const re_cards = await poker.getCards();
-
-        console.log(re_cards);
-
-        // expect(cards).to.equal(gamid);
-    })
-    // open card
-    it('open card ', async () => {
-
-        const { poker, owner, addr1, addr2 } = await loadFixture(
-            deployTokenFixture
-        );
-
-        let arr1 = [];
-
-        for (let i = 52; i >= 1; i--) {
-            arr1.push(ethers.hexlify(ethers.toUtf8Bytes(i.toString())))
-        }
-
-        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes("key")), arr1);
-
-        const cards = await poker.getCards();
-
-        const openCards = await poker.openCard(ethers.hexlify(ethers.toUtf8Bytes("key")), 0);
-
-    })
     // ROUND POKER
     it('ROUND POKER', async () => {
+        const { poker, owner, addr1, addr2 } = await loadFixture(
+            deployTokenFixture
+        );
+        // poker game create
+        let cardArr = [];
+        for(let i = 1; i <= 52; i++) {
+            cardArr.push(ethers.hexlify(ethers.toUtf8Bytes(i.toString())))
+        }
+        await poker.createGame(cardArr);
 
+        // players
+        let andrija = { name: "andrija", pk: "key1" };
+        let kobi = { name: "kobi", pk: "key2" };
+        let nico = { name: "nico", pk: "key3" };
+        let tom = { name: "tom", pk: "key4" };
+        
+        // all player shuffle encrypt all cards with key
+        // andrija shuffle encrypt
+        let cards1 = await poker.getCards();
+        let ncards = [];
+        for(let i = 0; i < 52; i++) ncards[i] = cards1[i];
+        for(let i = 51; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)) % 52;
+            [ncards[i], ncards[j]] = [ncards[j], ncards[i]];
+        }
+        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes(andrija.pk)), ncards);
+        // kobi shuffle encrypt
+        let cards2 = await poker.getCards();
+        for(let i = 0; i < 52; i++) ncards[i] = cards2[i];
+        for(let i = 51; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)) % 52;
+            [ncards[i], ncards[j]] = [ncards[j], ncards[i]];
+        }
+        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes(kobi.pk)), ncards);
+        // nico shuffle encrypt
+        let cards3 = await poker.getCards();
+        for(let i = 0; i < 52; i++) ncards[i] = cards3[i];
+        for(let i = 51; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)) % 52;
+            [ncards[i], ncards[j]] = [ncards[j], ncards[i]];
+        }
+        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes(nico.pk)), ncards);
+        // tom shuffle encrypt
+        let cards4 = await poker.getCards();
+        for(let i = 0; i < 52; i++) ncards[i] = cards4[i];
+        for(let i = 51; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)) % 52;
+            [ncards[i], ncards[j]] = [ncards[j], ncards[i]];
+        }
+        await poker.shuffleEncrypt(ethers.hexlify(ethers.toUtf8Bytes(tom.pk)), ncards);
+
+        // all player deprypt cards other than their own
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(andrija.pk)), 1);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(andrija.pk)), 2);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(andrija.pk)), 3);
+
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(kobi.pk)), 0);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(kobi.pk)), 2);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(kobi.pk)), 3);
+
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(nico.pk)), 0);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(nico.pk)), 1);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(nico.pk)), 3);
+
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(tom.pk)), 0);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(tom.pk)), 1);
+        await poker.revealCard(ethers.hexlify(ethers.toUtf8Bytes(tom.pk)), 2);
+
+        // each player open cards
+        let andrija_card = poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(andrija.pk)), 0);
+        let kobi_card = poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(kobi.pk)), 1);
+        let nico_card = poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(nico.pk)), 2);
+        let tom_card = poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(tom.pk)), 3);
+        
+        /*  add custom logic of a game:
+            1. swap card
+            2. place a bet
+            3. ...
+        */
+
+        // finally each player epen cards
+        let andrija_last_card = await poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(andrija.pk)), 0);
+        let kobi_last_card = await poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(kobi.pk)), 1);
+        let nico_last_card = await poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(nico.pk)), 2);
+        let tom_last_card = await poker.openCard(ethers.hexlify(ethers.toUtf8Bytes(tom.pk)), 3);
+        
+        console.log(`andrija: ${andrija_last_card}`);
+        console.log(`kobi: ${kobi_last_card}`);
+        console.log(`nico: ${nico_last_card}`);
+        console.log(`tom: ${tom_last_card}`);
+
+        // end game
+        poker.endGame();
     })
 
 });
